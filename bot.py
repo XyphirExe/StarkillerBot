5e5035bce745d7b02f3efee54e7c5c744f3a970e
Alan GARCIA <a.garcia.g007@gmail.com>
2019-03-19 21:24:35 +0100

    Changed command prefix from 'staar' to 'star'

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 
 TOKEN = 'NTU2OTMyMjgzMTM1OTUwODQ5.D3A_Uw.pr3hw_8BQFRWvYIMHpUxVgo0nnE'
 
-client = commands.Bot(command_prefix = 'staar')
+client = commands.Bot(command_prefix = 'star')
 
 @client.event
 async def on_ready():
